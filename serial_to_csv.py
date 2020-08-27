19a72568bb14ab4768092ef1be1dd382d5cf598c
colin.roos <PyQ3KZ6y6GpxrMz>
2020-08-27 17:59:54 -0400

    Setup for force and displacement measurement.

--- serial_to_csv.py
+++ serial_to_csv.py
@@ -9,18 +9,23 @@ port = serial.Serial('COM4')
 
 df = pd.DataFrame()
 df['time'] = 0
-df['mV'] = 0
+df['force_mV'] = 0
+df['displacement_mV'] = 0
 initial_reading = False
 start_time = 0
 elapsed_time = 0
 
-while not initial_reading or (initial_reading and elapsed_time < 10):
+while not initial_reading or (initial_reading and elapsed_time < 20):
 
     elapsed_time = float(perf_counter() - start_time)
 
     if port.inWaiting() != 0:
         try:
-            data = float(port.readline())
+            data = str(port.readline())
+
+            s = data.split('\\t')
+            force = s[0][2:]
+            position = s[1][:3]
 
             if not initial_reading:
                 initial_reading = True
@@ -29,8 +34,8 @@ while not initial_reading or (initial_reading and elapsed_time < 10):
         except ValueError:
             continue
 
-        print(data, elapsed_time)
-        new_row = pd.Series(data={'time': elapsed_time, 'mV': data})
+        print(force, position, elapsed_time)
+        new_row = pd.Series(data={'time': elapsed_time, 'force_mV': force, 'displacement_mV': position})
 
         df = df.append(new_row, ignore_index=True)
 
